d6537d331402856562f20992e21d996602c340dc
Vidya Mani <sm.vidya@gmail.com>
2017-09-29 20:03:40 -0500

    Function breif

--- Bigram.py
+++ Bigram.py
@@ -5,11 +5,27 @@ Submitted by : Vidya Sri Mani
 vxm163230
 """
 
+from collections import Counter
+import re
 
-#read file
-file = open('C:\\Users\\Vidya\\Desktop\\NLP\\Homework2\\corpus.txt')
-content = file.readlines()
 
-#read the file line by line and tokenize each word(space separated)
-for line in content:
-    words=line.split()
+#get the number of token(and frequencey of each space/newline separated word)
+def getTokenCount():
+
+
+def getBigramCount():
+
+
+def getBigramProb():
+
+
+
+#input file
+file ='C:\\Users\\Vidya\\Desktop\\NLP\\Homework2\\corpus.txt'
+content=open(file).read()
+#Split words (space separated)
+words = re.findall('[\n\S]+', content)
+
+c = (Counter(zip(words, words[1:])))
+print ('Hello')
+
