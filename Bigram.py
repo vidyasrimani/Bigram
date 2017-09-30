0b544a1e612a333437744fa7b4120402a772678c
Vidya Mani <sm.vidya@gmail.com>
2017-09-30 04:27:30 -0500

    Function breif

--- Bigram.py
+++ Bigram.py
@@ -6,21 +6,19 @@ vxm163230
 """
 
 from collections import Counter
-import re
 
 totalWords = 0
-testSentence = """The memo
-attempts to remove the tourist board as far as possible from the
-agency"""
+testSentence = """retired vice president and treasurer of"""
 
-#get the number of token(and frequencey of each token space/newline separated word)
-#This is also the unigram count
-
-
+#writes the unigram and bigram count list to file
 def writeToFile(filename,countList):
     with open(filename, 'w') as f:
         for count in countList:
             f.write((str(count)+'  -->  '+str(countList[count])+"\n"))
+
+
+#get the number of token(and frequencey of each token space/newline separated word)
+#This is also the unigram count
 def getTokenCount(file):
     print('Getting Token count...')
     content = open(file).read()
@@ -28,7 +26,9 @@ def getTokenCount(file):
     # Split words (space separated)
     #words = re.findall('\S+', content)
     words = content.split()
+
     totalWords = len(words)
+
     wordCount = {}
     wordCount = Counter(words)
 
@@ -40,7 +40,7 @@ def getBigramCount(words,unigramCount):
     print('Getting Bigram count...')
     bigramCount = {}
     #store word and next word in BigramCount
-    bigramCount = (Counter(zip(words,words[1:])))
+    bigramCount = (Counter(zip(words[1:],words)))
 
     writeToFile('C:\\Users\\Vidya\\Desktop\\NLP\\Homework2\\bigramCount.txt', bigramCount)
     print('Unigram and Bigram counts written to file')
@@ -48,26 +48,36 @@ def getBigramCount(words,unigramCount):
     getBigramProb(testSentence.split(), unigramCount, bigramCount)
 
 def getBigramProb(testSentencewords, unigramCount, bigramCount):
-    print('Test Sentence words without smoothing...')
-
-    #unzipping words from bigramCount.keys()
-    #ord1,word2=zip(*(bigramCount.keys()))
 
     testSentencewordCount = len(testSentencewords)
     testSentenceBigramCount = {}
+    bigramProbability = 1
+    probabiilty=1
+
 
     if(testSentencewordCount<2):
         print ('Enter a sentence with atleast two words')
     else:
         print('Calculating probabilities')
-        testSentenceBigram = Counter(zip(testSentencewords, testSentencewords[1:]))
-        for pair in testSentenceBigram.keys():
-            if (pair  in bigramCount.keys()):
-                print(pair)
-        else:
-            print('okay')
+        testSentenceBigramCount = Counter(zip(testSentencewords[1:],testSentencewords))
 
-        writeToFile('C:\\Users\\Vidya\\Desktop\\NLP\\Homework2\\testSentenceBigramCount.txt', testSentenceBigramCount)
+        #Calculating probabilty without smoothing
+        print('Calculating Probabilty without smoothing')
+        for pair in testSentenceBigramCount.keys():#pair has every bigram in the test sentence
+            if (pair  in bigramCount.keys()):
+                numerator = bigramCount[pair] #count of the bigram
+                denominator = unigramCount[pair[1]]#pair[1] gives the previous word in the bigram pair
+
+            else:
+                numerator=0
+            if denominator!= 0:
+                probability = numerator/denominator
+            bigramProbability *=probability
+
+        print('---------------------------------------')
+        print('Test sentence: ' + testSentence)
+        print('Bigram Probabilty without smoothing : ')
+        print(bigramProbability)
 
 #PROGRAM EXECUTION BEGINS HERE
 #input file
