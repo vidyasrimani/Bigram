7191a12107bbffb06f06c453cf460519d5a853d6
Vidya Mani <sm.vidya@gmail.com>
2017-09-29 21:55:22 -0500

    Function breif

--- Bigram.py
+++ Bigram.py
@@ -10,22 +10,38 @@ import re
 
 
 #get the number of token(and frequencey of each space/newline separated word)
-def getTokenCount():
+def getTokenCount(file):
+    print('Getting Token count...')
+    content = open(file).read()
+
+    # Split words (space separated)
+    words = re.findall('\S+', content)
+    wordCount = {}
+    for word in words:
+        if word in wordCount:
+            wordCount[word]+=1
+        else:
+            wordCount[word] = 1
+
+    getBigramCount(words)
 
 
-def getBigramCount():
 
+def getBigramCount(words):
+    print('Getting Bigram count...')
+    c = (Counter(zip(words, words[1:])))
 
 def getBigramProb():
+    print('getting Bigram Probabilty...')
 
 
-
+#PROGRAM EXEVUTION BEGINS HERE
 #input file
 file ='C:\\Users\\Vidya\\Desktop\\NLP\\Homework2\\corpus.txt'
-content=open(file).read()
-#Split words (space separated)
-words = re.findall('[\n\S]+', content)
 
-c = (Counter(zip(words, words[1:])))
-print ('Hello')
+#file=input("Enter file path")
+getTokenCount(file)
+#getBigramProb()
+
+print ('Done')
 
