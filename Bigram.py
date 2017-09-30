ad8e7c47b0d1e9b90885114d13c4b980d896ad9f
Vidya Mani <sm.vidya@gmail.com>
2017-09-29 22:37:38 -0500

    Function breif

--- Bigram.py
+++ Bigram.py
@@ -8,38 +8,54 @@ vxm163230
 from collections import Counter
 import re
 
+totalWords = 0
 
-#get the number of token(and frequencey of each space/newline separated word)
+#get the number of token(and frequencey of each token space/newline separated word)
+#This is also the unigram count
 def getTokenCount(file):
     print('Getting Token count...')
     content = open(file).read()
 
     # Split words (space separated)
     words = re.findall('\S+', content)
+    totalWords = len(words)
     wordCount = {}
+
     for word in words:
         if word in wordCount:
-            wordCount[word]+=1
+            wordCount[word] += 1
         else:
             wordCount[word] = 1
 
+    #Write Contents to file
     getBigramCount(words)
-
 
 
 def getBigramCount(words):
     print('Getting Bigram count...')
-    c = (Counter(zip(words, words[1:])))
+    bigramCount = {}
+    #bigrams = Counter(zip(words, nextword))
+    #store word and next word in BigramCount
+    for i in range (0,totalWords-1):
+        #Note to self:use Re for newline search
+        if(words[i]+" "+words[i+1]) in bigramCount:#already present
+            bigramCount[words[i] + " " + words[i + 1]] += 1
+        else:
+            bigramCount[words[i] + " " + words[i + 1]] += 1
+    #write content to file
 
 def getBigramProb():
     print('getting Bigram Probabilty...')
 
 
-#PROGRAM EXEVUTION BEGINS HERE
+#PROGRAM EXECUTION BEGINS HERE
 #input file
 file ='C:\\Users\\Vidya\\Desktop\\NLP\\Homework2\\corpus.txt'
+#file=input("Enter file path to corpus")
 
-#file=input("Enter file path")
+#testSentence = input('Enter Test Sentence')
+testSentence = ''
+
 getTokenCount(file)
 #getBigramProb()
 
